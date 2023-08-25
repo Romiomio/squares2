3554756d7a3431a815ef104992765a5abbffb1ee
Romiomio <90802238+Romiomio@users.noreply.github.com>
2023-08-25 22:53:21 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "22853340caf8bb58a81fb6d9c6614e59",
-"/": "22853340caf8bb58a81fb6d9c6614e59",
-"main.dart.js": "a18fed1fad23391a6c44d4d3f6102fc6",
+"index.html": "3118571132b4f7426df532c1fc79f01b",
+"/": "3118571132b4f7426df532c1fc79f01b",
+"main.dart.js": "6217aeb064e4c50556e6d235afae1d67",
 "manifest.json": "6eab1d1d7c42bc2fc4b897e7b9d29eef",
 "version.json": "14abe06c1555d46581bf96194f4d6664"
 };
